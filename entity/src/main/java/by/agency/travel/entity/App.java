18ea1e56a21d1b6633522930d996d4497faba6ce
dmitriiiv <dimkaiv@list.ru>
2015-08-03 11:07:13 +0300

    project structure initialization

---
+++ entity/src/main/java/by/agency/travel/entity/App.java
@@ -0,0 +1,13 @@
+package by.agency.travel.entity;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ service/src/main/java/by/agency/travel/service/App.java
@@ -0,0 +1,13 @@
+package by.agency.travel.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

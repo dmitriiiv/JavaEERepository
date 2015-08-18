6a89bb86a7d1e705db4db2067e2b519a6d5d7da7
dmitriiiv <dimkaiv@list.ru>
2015-08-18 14:48:37 +0300

    add new scripts

--- scripts/travel-run.bat
+++ scripts/travel-run.bat
@@ -1,11 +1,8 @@
 MKDIR d:\project\
 cd /d d:\project\
-git clone -b develop git@github.com:dmitriiiv/JavaEERepository.git
+git clone -b feature git@github.com:dmitriiiv/JavaEERepository.git
 
 mysql.exe -u root -p < d:\project\JavaEERepository\scripts\travel.sql
 
 cd /d d:\project\JavaEERepository\
 call mvn clean install
-
-cd dao\target\
-java -jar dao-1.0.jar
